05484b1f2ca3296011ca630aae41735eac3267a3
miskogepa <milovan.jankovic50m@gmail.com>
2025-04-15 16:04:56 +0200

    uzimanje id elementa iz HTML-a

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+// uzimanje id elementa iz HTML-a
+const cityInput = document.getElementById("city-input");
+const getWeatherButton = document.getElementById("get-weather-button");
+const weatherInfo = document.getElementById("weather-info");
+const cityNameDisplay = document.getElementById("city-name");
+const temperatureDisplay = document.getElementById("temperature");
+const descriptionDisplay = document.getElementById("description");
+const erorMessage = document.getElementById("error-message");
+
+})
